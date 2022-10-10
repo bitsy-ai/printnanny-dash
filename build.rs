44f9ac7fef81c6f5b14a16e0efc05bdc71f433f4
Leigh Johnson <leigh@bitsy.ai>
2022-10-10 16:58:14 -0700

    passthrough env vars to cross

--- build.rs
+++ build.rs
@@ -1,13 +1,17 @@
 use static_files::NpmBuild;
+use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-    NpmBuild::new("./ui")
+    let base_dir = env!("CARGO_MANIFEST_DIR");
+    let ui_dir = PathBuf::from(base_dir).join("ui");
+    let dist_dir = ui_dir.join("dist");
+    NpmBuild::new(ui_dir)
         .executable("npm")
         .install()
         .expect("Failed to run npm install")
         .run("build")
         .expect("Failed to run npm build")
-        .target("./ui/dist")
+        .target(dist_dir)
         .to_resource_dir()
         .build()
 }
