8c8d4ab9d25ea9d895eb765bec000586c633e557
Leigh Johnson <leigh@bitsy.ai>
2022-10-10 17:25:36 -0700

    remove workspace

--- tools/ci/build.bash
+++ tools/ci/build.bash
@@ -15,8 +15,8 @@ required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
 
 if [ -z "$RELEASE_BUILD" ]; then
-    $CROSS build --workspace --verbose --target $TARGET_TRIPLE
-    $CROSS build --workspace --verbose --target $TARGET_TRIPLE --all-features
+    $CROSS build --target $TARGET_TRIPLE
+    $CROSS build --target $TARGET_TRIPLE --all-features
 else
-    $CROSS build --workspace --verbose --target $TARGET_TRIPLE --all-features --release
+    $CROSS build --verbose --target $TARGET_TRIPLE --all-features --release
 fi
