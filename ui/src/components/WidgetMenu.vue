6cc4e2e7a1cd78d2dc96cd8fc50473759df179e7
Leigh Johnson <leigh@bitsy.ai>
2022-10-16 10:24:29 -0700

    add quick links

--- ui/src/components/WidgetMenu.vue
+++ ui/src/components/WidgetMenu.vue
@@ -4,7 +4,7 @@
       <MenuButton
         class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
       >
-        Options
+        Quick Links
         <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
       </MenuButton>
     </div>
