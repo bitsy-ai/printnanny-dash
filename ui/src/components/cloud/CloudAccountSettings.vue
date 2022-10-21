ed32d406d17531f0db0e3e6dd3ee0289805dcd9a
Leigh Johnson <leigh@bitsy.ai>
2022-10-20 20:46:26 -0700

    rm un-minified plotly

---
+++ ui/src/components/cloud/CloudAccountSettings.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="flex w-full">
+    
+</div>
+</template>
+
+<script setup lang="ts">
+import { useCloudStore } from "@/stores/cloud";
+
+const cloudStore = useCloudStore();
+</script>
--- ui/src/views/SettingsView.vue
+++ ui/src/views/SettingsView.vue
@@ -5,10 +5,22 @@
         <h1 class="text-3xl font-bold text-gray-900 prose">{{ pageTitle }}</h1>
       </div>
     </header>
-    <main></main>
+    <main>
+      <div class="mx-auto max-w-7xl sm:p-6 lg:p-8">
+        <h2 class="text-xl font-bold text-gray-900 prose">
+          PrintNanny Cloud
+        </h2>
+        <div
+          class="rounded-lg border-4 border-dashed border-gray-200 grid grid-cols-1 md:grid-cols-3 p-4 gap-4 justify-evenly"
+        >
+          <CloudAccountSettings />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import CloudAccountSettings from "@/components/cloud/CloudAccountSettings.vue"
 const pageTitle = "⚙️ Settings";
 </script>
