3a5c3a27c99285daeef770b0a6602ded82f433db
Leigh Johnson <leigh@bitsy.ai>
2022-10-21 22:02:22 -0700

    add posthod + identify user by email

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,6 +2,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import posthog from 'posthog-js'
 
 import App from "./App.vue";
 import router from "./router";
@@ -16,4 +17,8 @@ pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.use(router);
 
+if (!window.location.href.includes('127.0.0.1') && !window.location.href.includes('localhost')) {
+    posthog.init(import.meta.env.VITE_POSTHOG_CLIENT_KEY, { api_host: import.meta.env.VITE_POSTHOG_API_URL })
+}
 app.mount("#app");
+
