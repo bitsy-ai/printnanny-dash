9adb3c94a0ecd96f23e8d5368703ebfdc79191a4
Tricia Bogen <tricia-github@cernu.us>
2022-11-17 22:10:47 -0800

    tidied up rust side of things

--- src/get_config_data.rs
+++ src/get_config_data.rs
@@ -1,43 +1,41 @@
 use actix_web::{get, web, Responder};
 use log::error;
 use serde::Serialize;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 #[derive(Serialize)]
-struct ConfigFile<'a> {
-    filename: &'a str,
-    path: &'a str,
-    content: &'a mut String,
-    syntax: &'a str,
+struct ConfigFile {
+    filename: String,
+    path: String,
+    content: String,
+    syntax: String,
 }
 
 #[get("/printnanny/api/pi/configs")]
 pub async fn get_config_data() -> impl Responder {
-    const FILES: &mut [ConfigFile] = &mut [
-        ConfigFile {
-            filename: "PrintNannyConfig.toml",
-            path: "unknown",
-            content: &mut String::new(),
-            syntax: "toml",
-        },
-        ConfigFile {
-            filename: "config.yaml",
-            path: "/home/printnanny/.octoprint/config.yaml",
-            content: &mut String::new(),
-            syntax: "yaml",
-        },
+    const FILES: &[(&'static str, &'static str, &'static str)] = &[
+        ("PrintNannyConfig.toml", "missing", "toml"),
+        (
+            "octoprint config",
+            "/home/printnanny/.octoprint/config.yaml",
+            "yaml",
+        ),
     ];
 
-    for file in FILES.iter_mut() {
-        let result = File::open(file.path).map(|mut f| f.read_to_string(&mut file.content));
-        if result.is_err() {
-            error!("could not open {:?}: {:?}", file.path, result.err());
-            file.content.clear();
-            file.content.push_str("could not open ");
-            file.content.push_str(file.path);
-        }
-    }
+    let mut files: Vec<ConfigFile> = Vec::new();
 
-    web::Json(FILES)
+    for file in FILES {
+        files.push(ConfigFile {
+            filename: file.0.to_string(),
+            path: file.1.to_string(),
+            syntax: file.2.to_string(),
+            content: fs::read_to_string(file.1).unwrap_or_else(|err| {
+                error!("could not read file {:?}: {:?}", file.1, err);
+                let mut placeholder = String::from("could not read file ");
+                placeholder.push_str(file.1);
+                placeholder
+            }),
+        });
+    }
+    web::Json(files)
 }
