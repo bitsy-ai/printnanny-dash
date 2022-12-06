5614ffd97d10bcfbf0cdb353d350b30c247a1176
Leigh Johnson <leigh@bitsy.ai>
2022-12-06 14:41:19 -0800

    remove HTTP-based DeviceInfo queries

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 const GIT_VERSION: &str = git_version!();
 
 mod get_config_data;
-mod get_version_data;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -23,7 +22,6 @@ async fn main() -> Result<()> {
         let generated = generate();
         App::new()
             .service(get_config_data::get_config_data)
-            .service(get_version_data::get_version_data)
             .service(ResourceFiles::new("/*", generated).resolve_not_found_to_root())
     })
     .workers(config.workers)
