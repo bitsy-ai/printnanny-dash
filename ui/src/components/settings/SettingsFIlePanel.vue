71843a282d733d284951fb6416ebec01b126b42f
Leigh Johnson <leigh@bitsy.ai>
2022-12-07 12:47:19 -0800

    wip: hook up codemirror

---
+++ ui/src/components/settings/SettingsFIlePanel.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="divide-y divide-gray-200 lg:col-span-9">
+    </div>
+</template>
--- ui/src/components/settings/SettingsFileEditor.vue
+++ ui/src/components/settings/SettingsFileEditor.vue
@@ -1,4 +1,26 @@
-<template></template>
+<template>
+
+    <codemirror v-for="file in store.settingsFiles"
+        :key="file.file_name"
+        v-model="file.content"
+        :style="{ height: '400px' }"
+        :autofocus="true"
+        :indent-with-tab="true"
+        :tab-size="2"
+    />
+</template>
 <script setup lang="ts">
+import type { PropType } from "vue";
+import { useSettingsFileStore } from "@/stores/settingsFile";
+import type { WidgetItem } from "@/types";
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<WidgetItem>,
+    required: true,
+  },
+});
+
+const store = useSettingsFileStore(props.item);
 
 </script>
