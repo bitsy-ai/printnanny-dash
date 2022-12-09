5fde34b02cd8857aa8a9760840bcd5dfa10c6da7
Leigh Johnson <leigh@bitsy.ai>
2022-12-09 09:31:08 -0800

    Remove rust actix app and rust build/release CI (#62)
    * remove rust actix app, rust build/release CI
    
    * fix checkout line

--- env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! [ -d node ]; then
-  mkdir node
-  curl -L https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz \
-  | tar -xJ --strip-components=1 -C node
-fi
-
-export PATH="$PWD/node/bin:$PATH"
