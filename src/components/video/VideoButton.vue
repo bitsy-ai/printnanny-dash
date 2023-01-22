5d7c817424921d2409717596b1b18592f6ffae0f
Leigh Johnson <leigh@bitsy.ai>
2023-01-22 08:27:31 -0800

    make red button

--- src/components/video/VideoButton.vue
+++ src/components/video/VideoButton.vue
@@ -11,17 +11,21 @@
       <button
         v-if="store.status == ConnectionStatus.ConnectionLoading"
         disabled
-        class="ml-3 inline-flex disabled:opacity-25 justify-center focus:outline-none rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm"
+        class="ml-3 inline-flex disabled:opacity-50 justify-center focus:outline-none rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm"
       >
-        Loading...
+      <VideoCameraIcon class="w-5 h-5 mr-2"></VideoCameraIcon>
+
+        Loading
       </button>
 
       <button
         disabled
         v-else-if="store.status == ConnectionStatus.ConnectionClosing"
-        class="ml-3 inline-flex disabled:opacity-25 justify-center focus:outline-none rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm"
+        class="ml-3 inline-flex disabled:opacity-50 justify-center focus:outline-none rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm"
       >
-        Stopping...
+      <VideoCameraIcon class="w-5 h-5 mr-2"></VideoCameraIcon>
+
+        Stopping
       </button>
 
       <!-- camera service is offline -->
@@ -56,7 +60,7 @@
           store.videoRecordingButtonShowStart &&
           store.videoRecordingButtonEnabled
         "
-        class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <PlayCircleIcon class="w-5 h-5 mr-2"></PlayCircleIcon>
         {{ store.videoRecordingButtonText }}
@@ -67,7 +71,7 @@
           !store.videoRecordingButtonShowStart &&
           store.videoRecordingButtonEnabled
         "
-        class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <StopCircleIcon class="w-5 h-5 mr-2"></StopCircleIcon>
         {{ store.videoRecordingButtonText }}
@@ -75,7 +79,7 @@
       <button
         disabled
         v-else
-        class="disabled:cursor-not-allowed disabled:opacity-50 ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="disabled:cursor-not-allowed disabled:opacity-50 ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <PlayCircleIcon class="w-5 h-5 mr-2"></PlayCircleIcon>
         Start Recording
