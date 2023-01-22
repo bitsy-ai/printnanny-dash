ec6c4531d9d88fc9f1bd82eed139928974ec90ec
Leigh Johnson <leigh@bitsy.ai>
2023-01-21 17:49:02 -0800

    fix state of recording button

--- src/components/video/VideoButton.vue
+++ src/components/video/VideoButton.vue
@@ -52,25 +52,19 @@
       <!-- camera service is active and recording is not active -->
       <button
         @click="store.startRecording"
-        v-if="
-          store.status == ConnectionStatus.ConnectionReady &&
-          store.currentVideoRecording === undefined
-        "
+        v-if="store.videoRecordingButtonShowStart && store.videoRecordingButtonEnabled"
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
         <PlayCircleIcon class="w-5 h-5 mr-2"></PlayCircleIcon>
-        Start Recording
+        {{  store.videoRecordingButtonText }}
       </button>
       <button
         @click="store.stopRecording"
-        v-else-if="
-          store.status == ConnectionStatus.ConnectionReady &&
-          store.currentVideoRecording !== undefined
-        "
+        v-else-if="!store.videoRecordingButtonShowStart && store.videoRecordingButtonEnabled"
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
         <PlayCircleIcon class="w-5 h-5 mr-2"></PlayCircleIcon>
-        Stop Recording
+        {{  store.videoRecordingButtonText }}
       </button>
       <button
         disabled
