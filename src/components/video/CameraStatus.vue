8918bdb84118d94363ca73d3f111614beed096d1
Leigh Johnson <leigh@bitsy.ai>
2023-02-15 17:13:21 +0100

    fix camera status styles

--- src/components/video/CameraStatus.vue
+++ src/components/video/CameraStatus.vue
@@ -10,25 +10,31 @@
       class="flex items-center space-x-3 font-medium text-gray-600 m-auto"
       v-else-if="store.cameraStatus?.recording"
     >
-      <VideoCameraIcon
-        class="bg-red-500 flex-shrink-0 w-2.5 h-2.5 rounded-full"
+      <PlayCircleIcon
+        class="text-red-500 flex-shrink-0 w-5 h-5"
         aria-hidden="true"
         alt="Recording in progress"
-      ></VideoCameraIcon>
-      <div
-        class="bg-red-500 flex-shrink-0 w-2.5 h-2.5 rounded-full"
-        aria-hidden="true"
-      ></div>
+      ></PlayCircleIcon>
     </div>
     <div
       class="flex items-center space-x-3 font-medium text-gray-600 m-auto"
       v-else-if="store.cameraStatus?.streaming"
     >
       <VideoCameraIcon
-        class="bg-green-500 flex-shrink-0 w-2.5 h-2.5 rounded-full"
+        class="text-green-500 flex-shrink-0 w-5 h-5"
         aria-hidden="true"
         alt="Camera stream active"
       ></VideoCameraIcon>
+    </div>
+    <div
+      class="flex items-center space-x-3 font-medium text-gray-600 m-auto"
+      v-else
+    >
+      <PauseCircleIcon
+        class="text-amber-500 flex-shrink-0 w-5 h-5"
+        aria-hidden="true"
+        alt="Camera stream is paused"
+      ></PauseCircleIcon>
     </div>
   </Transition>
 </template>
@@ -45,13 +51,18 @@
 </style>
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { VideoCameraIcon } from "@heroicons/vue/20/solid";
+import {
+  PlayCircleIcon,
+  VideoCameraIcon,
+  PauseCircleIcon,
+} from "@heroicons/vue/20/solid";
 import { ConnectionStatus } from "@/types";
 import { useVideoStore } from "@/stores/video";
 import TextSpinner from "@/components/TextSpinner.vue";
 
 const store = useVideoStore();
 const loading = ref(true);
+
 onMounted(async () => {
   await store.loadCameraStatus();
   loading.value = false;
