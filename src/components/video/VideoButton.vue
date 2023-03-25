71860b4bda33924a57778e43ad32f077502c7be6
Leigh Johnson <leigh@bitsy.ai>
2023-03-25 10:37:39 -0700

    fix start recording button indicator

--- src/components/video/VideoButton.vue
+++ src/components/video/VideoButton.vue
@@ -44,24 +44,22 @@
       <button
         @click="store.startRecording"
         v-else-if="
-          store.videoRecordingButtonShowStart &&
-          store.videoRecordingButtonEnabled
+          store.cameraStatus?.streaming && !store.cameraStatus?.recording
         "
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <PlayCircleIcon class="w-5 h-5 mr-2"></PlayCircleIcon>
-        {{ store.videoRecordingButtonText }}
+        Start Recording
       </button>
       <button
         @click="store.stopRecording"
         v-else-if="
-          !store.videoRecordingButtonShowStart &&
-          store.videoRecordingButtonEnabled
+          store.cameraStatus?.streaming && store.cameraStatus?.recording
         "
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2"
       >
         <StopCircleIcon class="w-5 h-5 mr-2"></StopCircleIcon>
-        {{ store.videoRecordingButtonText }}
+        Stop Recording
       </button>
       <button
         disabled
