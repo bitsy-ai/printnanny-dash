e174e678861ccb1ec5c087116aea7dd504918499
Leigh Johnson <leigh@bitsy.ai>
2023-07-13 17:03:18 -0700

    add Crash Report/Settings icon link text

--- src/components/nav/TopNav.vue
+++ src/components/nav/TopNav.vue
@@ -58,7 +58,7 @@
           </DisclosureButton>
         </div>
         <!-- nats connection status indicator -->
-        <div class="hidden sm:ml-6 sm:flex sm:items-center">
+        <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
           <!-- 
             <button type="button" class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               <span class="sr-only">View notifications</span>
@@ -71,21 +71,21 @@
               router.currentRoute.value.name == 'Settings'
                 ? 'border-indigo-500 text-gray-900'
                 : 'border-transparent text-gray-500 hover:text-gray-700',
-              'inline-flex items-center px-1 pt-1 text-sm font-medium mr-4',
+              'cursor-pointer border-transparent border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
             ]"
           >
-            <Cog6ToothIcon class="h-6 w-6" />
+            <Cog6ToothIcon class="h-6 w-6 mr-2" />Settings
           </router-link>
           <a
             @click="alerts.openCrashReport"
             :class="[
-              'border-transparent text-gray-500 hover:text-gray-700',
+              'cursor-pointer border-transparent border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
               'inline-flex items-center px-1 pt-1 text-sm font-medium mr-4',
             ]"
           >
-            <FireIcon class="h-6 w-6" />
+            <FireIcon class="h-6 w-6 mr-2" />Crash Report
           </a>
-          <CloudSyncStatus />
+          <CloudSyncStatus class="justify-self-end"/>
         </div>
       </div>
     </div>
